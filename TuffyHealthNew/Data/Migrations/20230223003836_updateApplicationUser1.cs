21b8473e8f4411f4f28d3e8b6697d595b3e4889a
david fazio <davidfazio1102@gmail.com>
2023-02-22 22:14:41 -0800

    Updates to the database, all attributes are syncing now for register page

---
+++ TuffyHealthNew/Data/Migrations/20230223003836_updateApplicationUser1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TuffyHealthNew.Data.Migrations
+{
+    public partial class updateApplicationUser1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
